35985e90a6bc770c6df2e0a5582c07f7b5fcf00d
vijay-kannan <37103145+vijay-kannan@users.noreply.github.com>
2019-06-07 17:03:47 +0530

    Update and rename README.md to OddorEven.cpp

---
+++ OddorEven.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+Void main()
+{
+Int num
+Cout<<"Enter the Number";
+Cin>>num;
+If(num/2==0)
+{
+Cout<<"The number is even";
+}
+Else
+{
+Cout<<"The number is odd";
+}
+}
